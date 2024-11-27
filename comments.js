b07ef4016bf908b3d56edda1e163affa9ac6efd1
Maria Anees <mariaanees079@gmail.com>
2024-11-27 13:23:56 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,20 @@
+// Create web server
+// // Load the express module
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// // Create a route for the root path
+// app.get('/', (req, res) => {
+//     res.send('Welcome to the homepage!');
+// });
+
+// // Create a route for the /comment path
+// app.get('/comment', (req, res) => {
+//     res.send('Welcome to the comment page!');
+// });
+
+// // Start the web server
+// app.listen(port, () => {
+//     console.log(`Server running on http://localhost:${port}`);
+// });
